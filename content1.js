15efa6eef21428c2d125909d4a4c01bfc45c303e
Andrew <andrewdayongwang@gmail.com>
2017-05-15 16:26:40 -0400

    add start/end button functionality

--- content1.js
+++ content1.js
@@ -1,12 +1,45 @@
+var initialTime;
+
 $(document).ready(delayedChange());
 
 function delayedChange() {
-  timeoutID = window.setTimeout(updateHTML, 200);
+  timeoutID = window.setTimeout(updateHTML, 800);
 }
 
 function updateHTML() {
   console.log("here");
-  document.getElementById("startTitle").innerHTML = startName;
-  document.getElementById("endTitle").innerHTML = endName;
-  document.getElementById("startButton").setAttribute('action', startURL);
+  document.getElementById("startTitle").innerHTML = "Start at: " + startName;
+  document.getElementById("endTitle").innerHTML = "Try to find: " + endName;
+  document.getElementById("startButton").onclick = function(){ startButton(); } ;
+  document.getElementById("endButton").onclick = function(){ endButton(); } ;
+}
+
+function startButton() {
+  initialTime = Date.now();
+  console.log(initialTime);
+  window.open(startURL,'_blank')
+}
+
+function endButton() {
+  checkTime();
+}
+
+function checkTime(){
+  var timeDifference = Date.now() - initialTime;
+  console.log(timeDifference);
+  var formatted = convertTime(timeDifference);
+  console.log(formatted);
+  document.getElementsByTagName("h5").innerHTML = '' + formatted;
+}
+
+function convertTime(milliseconds) {
+  var totalSeconds = Math.floor(milliseconds/1000);
+  var minutes = leftPad(Math.floor(totalSeconds/60),2); 
+  var seconds = leftPad(totalSeconds - minutes * 60,2); 
+  where: function leftPad (aNumber, aLength) { 
+    if (aNumber.toString().length >= aLength) {
+      return aNumber; 
+    }
+    return (Math.pow(10, aLength) + Math.floor(aNumber)).toString().substring(1); 
+  }
 }

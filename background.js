e67a4e0edfd16448b8f4e5ba0b9c0e268e227a8d
Andrew <andrewdayongwang@gmail.com>
2017-05-16 17:32:05 -0400

    automatic knows when you won

--- background.js
+++ background.js
@@ -35,6 +35,11 @@
 
 // found on http://stackoverflow.com/questions/3188384/google-chrome-extensions-open-new-tab-when-clicking-a-toolbar-icon
 
+
+var initialTime;
+var endURL;
+
+
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
     var newURL = "https://immense-stream-44778.herokuapp.com/";
@@ -46,13 +51,47 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
 // it must listen for the message from content1 telling it to start the timer
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    // console.log(sender.tab ?
-    //             "from a content script:" + sender.tab.url :
-    //             "from the extension");
-    // if (request.greeting == "start the timer")
+    if (request.greeting == "start the timer") {
     //   start_timer(); // function to start the timer
-      sendResponse({farewell: "it worked"});
+      initialTime = Date.now();
+    } 
+    // else if (request.greeting == "end the timer") {
+    //   sendResponse({farewell: checkTime()});
+    // }
+    else if (request.greeting == "current url") {
+      // sendResponse({farewell: request.content + "!!!!!" + endURL});
+      if (request.content == endURL || request.content == "https://tranquil-taiga-73034.herokuapp.com/index.html") {
+        sendResponse({farewell: checkTime()});
+      }
+    }
+    else if (request.greeting == "desired url") {
+      endURL = request.content;
+    }
+
   });
+
+function checkTime(){
+  var timeDifference = Date.now() - initialTime;
+  console.log(timeDifference);
+  var formatted = convertTime(timeDifference);
+  console.log(formatted);
+  return formatted;
+}
+
+function convertTime(milliseconds) {
+  var totalSeconds = Math.floor(milliseconds/1000);
+  var minutes = leftPad(Math.floor(totalSeconds/60),2); 
+  var seconds = leftPad(totalSeconds - minutes * 60,2); 
+  var milli = leftPad(milliseconds - totalSeconds * 1000, 3);
+  return minutes + ":" + seconds + ":" + milli;
+}
+
+function leftPad (aNumber, aLength) { 
+  if (aNumber.toString().length >= aLength) {
+    return aNumber; 
+  }
+  return (Math.pow(10, aLength) + Math.floor(aNumber)).toString().substring(1); 
+}
 
 // function start_timer() {
 //   console.log("function is executed")
--- popup.js
+++ popup.js
@@ -7,51 +7,48 @@
 //     console.log(tabUrl);
 // });
 
-var startID;
-var endID;
-var startName;
-var endName;
-var startURL;
-var endURL;
+var startID = "";
+var endID = "";
+var startName = "";
+var endName = "";
+var startURL = "";
+var endURL = "";
 
 $(document).ready(function() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=2", true);
-  xhr.send();
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4 && xhr.status == 200)  {
-      var resp = JSON.parse(xhr.responseText);
-      startID = resp.query.random[0].id;
-      endID = resp.query.random[1].id;
-      startName = resp.query.random[0].title;
-      endName = resp.query.random[1].title;
-      var startNameArr = startName.split([' ']);
-      var endNameArr = endName.split([' ']);
-      var startNameURL = "";
-      var endNameURL = "";
-      for (var i = 0; i < startNameArr.length; i++) {
-        startNameURL = startNameURL + startNameArr[i] + "_";
+  if (startID == "") {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=2", true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status == 200)  {
+        var resp = JSON.parse(xhr.responseText);
+        startID = resp.query.random[0].id;
+        endID = resp.query.random[1].id;
+        startName = resp.query.random[0].title;
+        endName = resp.query.random[1].title;
+        var startNameArr = startName.split([' ']);
+        var endNameArr = endName.split([' ']);
+        var startNameURL = "";
+        var endNameURL = "";
+        for (var i = 0; i < startNameArr.length; i++) {
+          startNameURL = startNameURL + startNameArr[i] + "_";
+        }
+        startNameURL = startNameURL.substring(0, startNameURL.length - 1);
+        for (var i = 0; i < endNameArr.length; i++) {
+          endNameURL = endNameURL + endNameArr[i] + "_";
+        }
+        endNameURL = endNameURL.substring(0, endNameURL.length - 1);
+        startURL = "https://en.wikipedia.org/wiki/" + startNameURL;
+        endURL = "https://en.wikipedia.org/wiki/" + endNameURL;
+        console.log(startID);
+        console.log(endID);
+        console.log(startName);
+        console.log(endName);
+        console.log(startURL);
+        console.log(endURL);
       }
-      startNameURL = startNameURL.substring(0, startNameURL.length - 1);
-      for (var i = 0; i < endNameArr.length; i++) {
-        endNameURL = endNameURL + endNameArr[i] + "_";
-      }
-      endNameURL = endNameURL.substring(0, endNameURL.length - 1);
-      startURL = "https://en.wikipedia.org/wiki/" + startNameURL;
-      endURL = "https://en.wikipedia.org/wiki/" + endNameURL;
-      console.log(startID);
-      console.log(endID);
-      console.log(startName);
-      console.log(endName);
-      console.log(startURL);
-      console.log(endURL);
     }
   }
-  $( "#start" ).click(function() {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-      chrome.tabs.update(tab.id, {url: "https://immense-stream-44778.herokuapp.com/"});
-    });
-  });
 });
 
 /* This stuff below was from StackOverflow */
